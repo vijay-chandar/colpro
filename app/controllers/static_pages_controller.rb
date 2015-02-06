045bd15f2a66df2159a0b6583e8f4da72a30153d
Vijay-Chandar <reachvijaychandar@gmail.com>
2015-02-06 14:21:03 +0530

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def call_for_papers
+  end
+
+  def important_dates
+  end
+
+  def paper_ethics
+  end
+
+  def committee
+  end
+
+  def venue_and_contact
+  end
+end
