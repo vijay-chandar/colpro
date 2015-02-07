aa75b6ae2cc5c398b4d1696c7ac21358c8e7ecd2
moinudeen <moinu.95@gmail.com>
2015-02-07 21:54:44 +0530

    Another attempt at  precompilation methods (hope it works)

--- config/application.rb
+++ config/application.rb
@@ -23,23 +23,5 @@ module Ncert2015
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.assets.precompile << Proc.new { |path|
-  if path =~ /\.(css|js)\z/
-    full_path = Rails.application.assets.resolve(path).to_path
-    app_assets_path = Rails.root.join('app', 'assets').to_path
-    vendor_assets_path = Rails.root.join('vendor', 'assets').to_path
-
-    if ((full_path.starts_with? app_assets_path) || (full_path.starts_with? vendor_assets_path)) && (!path.starts_with? '_')
-      puts "\t" + full_path.slice(Rails.root.to_path.size..-1)
-      true
-    else
-      false
-    end
-  else
-    false
-  end
-}
-
-
   end
 end
