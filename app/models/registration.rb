ca7f45f3cea44fa277ac88bf1f1739419ab84e2e
aravind <pro.aravind@gmail.com>
2015-02-08 00:26:21 +0530

    validation issue

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,7 +3,8 @@ class Registration < ActiveRecord::Base
 		:storage => :dropbox, 
 		:dropbox_credentials => Rails.root.join("config/dropbox.yml")
 	# do_not_validate_attachment_file_type :file_upload
-	validates_attachment :file_upload, content_type: ["application/pdf","application/docx","application/doc","application/odt"]
+	# validates_attachment :file_upload, content_type: ["application/pdf","application/docx","application/doc","application/odt"]
+	validates_attachment :file_upload,  :presence => true, content_type: { content_type: ["application/pdf", "application/docx", "application/doc", "application/odt"] }
 	enum department: [ :CSE, :ECE, :EEE, :IT, :MECH, :CIVIL, :Others ]
 	enum type_of_conference: [ "CSE & IT", "ECE & EEE", "MECH & CIVIL" ] 
 	validates :first_name, :last_name, :email, :college_name, :department, :paper_title, :abstract, presence: true
