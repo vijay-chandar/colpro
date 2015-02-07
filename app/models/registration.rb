1339a5caaa846dffc33f26a376e12ac5235000c9
aravind <pro.aravind@gmail.com>
2015-02-08 00:31:30 +0530

    added changes for download link and dropbox related

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,7 +1,8 @@
 class Registration < ActiveRecord::Base
 	has_attached_file :file_upload, 
 		:storage => :dropbox, 
-		:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+		:dropbox_credentials => Rails.root.join("config/dropbox.yml"), 
+		:dropbox_visibility => 'public'
 	# do_not_validate_attachment_file_type :file_upload
 	# validates_attachment :file_upload, content_type: ["application/pdf","application/docx","application/doc","application/odt"]
 	validates_attachment :file_upload,  :presence => true, content_type: { content_type: ["application/pdf", "application/docx", "application/doc", "application/odt"] }
