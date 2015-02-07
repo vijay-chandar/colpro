cb79de2552662f8f7b1ee0210bcd33c611448e28
aravind <pro.aravind@gmail.com>
2015-02-08 00:17:56 +0530

    added dropbox changes and added validation for fileupload

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,7 +1,9 @@
 class Registration < ActiveRecord::Base
-	has_attached_file :file_upload
-    do_not_validate_attachment_file_type :file_upload
-	# validates_attachment :file_upload, content_type: { content_type: ["application/pdf","application/docx","application/doc","application/odt"] }
+	has_attached_file :file_upload, 
+		:storage => :dropbox, 
+		:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+	# do_not_validate_attachment_file_type :file_upload
+	validates_attachment :file_upload, content_type: ["application/pdf","application/docx","application/doc","application/odt"]
 	enum department: [ :CSE, :ECE, :EEE, :IT, :MECH, :CIVIL, :Others ]
 	enum type_of_conference: [ "CSE & IT", "ECE & EEE", "MECH & CIVIL" ] 
 	validates :first_name, :last_name, :email, :college_name, :department, :paper_title, :abstract, presence: true
