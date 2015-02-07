96bdc9a4a9943185ee1f44a5f11fb672e52026c2
Vijay-Chandar <reachvijaychandar@gmail.com>
2015-02-07 11:17:52 +0530

    New Fields Added

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,7 +2,7 @@ class Registration < ActiveRecord::Base
 	has_attached_file :file_upload
     do_not_validate_attachment_file_type :file_upload
 	# validates_attachment :file_upload, content_type: { content_type: ["application/pdf","application/docx","application/doc","application/odt"] }
-	enum department: [ :CSE, :ECE, :EEE, :IT, :MECH ]
-	enum type_of_conference: [ "CSE & IT", "ECE & EEE", "MECHANICAL"] 
+	enum department: [ :CSE, :ECE, :EEE, :IT, :MECH, :CIVIL, :Others ]
+	enum type_of_conference: [ "CSE & IT", "ECE & EEE", "MECH & CIVIL" ] 
 	validates :first_name, :last_name, :email, :college_name, :department, :paper_title, :abstract, presence: true
 end
